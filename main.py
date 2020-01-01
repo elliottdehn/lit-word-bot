0034eb9af86a7422b78e2cb92a09e618510cf9c5
elliottdehnbostel <dehnbostele@gmail.com>
2019-12-31 19:21:36 -0600

    upgraded the word filtering algorithm and sped it up significantly. Going to work on the API algorithm which right now is very fragile and bad.

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import heapq
 import secrets as secrets
 from functools import reduce
 import glob
+import logging
 
 blacklist = [
     "anime",
@@ -28,6 +29,7 @@ blacklist = [
     "depression",
     "depthhub",
     "drinkingdollars",
+    "FemaleDatingStrategy",
     "forwardsfromgrandma",
     "geckos",
     "giraffes",
@@ -120,21 +122,22 @@ def clean(body):
 
 
 def filter_submissions(submissions, submission_score, hours):
-    submissions = filterfalse(lambda p: not scoreOver(
+    submissions = filter(lambda p: scoreOver(
         p, submission_score), submissions)
-    submissions = filterfalse(lambda p: not inPastHours(
+    submissions = filter(lambda p: inPastHours(
         p.created_utc, hours), submissions)
-    submissions = filterfalse(lambda p: p.stickied, submissions)
-    submissions = filterfalse(
-        lambda p: p.distinguished == "moderator", submissions)
+    submissions = filter(lambda p: not p.stickied, submissions)
+    submissions = filter(lambda p: p.subreddit.name not in blacklist, submissions)
+    submissions = filter(
+        lambda p: p.distinguished != "moderator", submissions)
     return submissions
 
 
 def filter_posts(posts, post_score, hours):
-    posts = filterfalse(lambda p: p.num_comments < 3, posts)
     posts = filter_submissions(posts, post_score, hours)
-    posts = filterfalse(lambda p: p in blacklist, posts)
-    posts = filterfalse(lambda p: p.locked, posts)
+    posts = filter(lambda p: p.num_comments >= 1, posts)
+    posts = filter(lambda p: not p.locked, posts)
+    posts = filter(lambda p: not p.over_18, posts)
     return posts
 
 
@@ -168,8 +171,8 @@ def split(left, elm_set):
 def nuples_to_map(nuple_set, key_idx):
     nuple_map = {nuple[key_idx]: set() for nuple in nuple_set}
     for key in nuple_map.keys():
-        nuple_map[key] = set(filterfalse(
-            lambda elm: elm[key_idx] != key, nuple_set))
+        nuple_map[key] = set(filter(
+            lambda elm: elm[key_idx] == key, nuple_set))
     return nuple_map
 
 # - select the highest scoring comment for each, resulting in (word -> Comment) mapping
@@ -220,19 +223,11 @@ def hot_all_word_map(reddit, lim, post_score, post_hours, comm_score, comm_hours
     bags = list()
     for sub_name in hot_subs.keys():
         sub = reddit.subreddit(sub_name)
-        print("Working on: " + sub_name)
-        bags.extend(hot_sub_comment_bags(sub, lim,
-                                         post_score, post_hours, comm_score, comm_hours))
+        if(not sub.over18):
+            print("Working on: " + sub_name)
+            bags.extend(hot_sub_comment_bags(sub, lim,
+                                             post_score, post_hours, comm_score, comm_hours))
     return map_word_to_comment(bags)
-
-
-def word_set_from_dir(directory, folder, encoding):
-    words = ''
-    for filename in next(walk(directory))[2]:
-        with io.open(folder + filename, "r", encoding=encoding, errors='ignore') as f:
-            for line in f:
-                words += clean(line) + ' '
-    return set(clean(words).split())
 
 
 def word_set_from_dir_no_clean(directory, folder, encoding):
@@ -250,71 +245,125 @@ def partition(items, predicate=bool):
             (item for pred, item in b if pred))
 
 
-def stem_set(word_set):
+def stem(word):
     stemmer = PorterStemmer()
-    return set([stemmer.stem(word) for word in word_set])
+    return stemmer.stem(word)
 
+
+def stem_set(word_set):
+    return set([stem(word) for word in word_set])
+
+
+def read_data(file_name, delim):
+    data = dict()
+    with open(file_name, "r", encoding="utf-8") as f:
+        for line in f:
+            l, r = line.split(delim)
+            data[l] = int(r)
+    return data
+
+
+def merge_entry(m, entry):
+    if (entry[0] not in m):
+        m[entry[0]] = entry[1]
+    else:
+        m[entry[0]] += entry[1]
+
+
+def keep_only(m, predicate):
+    m2 = dict()
+    for k in m.keys():
+        if predicate(k):
+            m2[k] = m[k]
+    return m2
+
+
+def merge_map(m1, m2, collision_f):
+    m3 = dict()
+    for k, v in m1.items():
+        m3[k] = v
+    for k, v in m2.items():
+        if (k in m3):
+            m3[k] = collision_f(m1[k], v)
+        else:
+            m3[k] = v
+    return m3
+
+
+def word_freq_to_stem_freq(word_freqs):
+    stem_freqs = dict()
+    for word in word_freqs.keys():
+        merge_entry(stem_freqs, (stem(word), word_freqs[word]))
+    return stem_freqs
+
+
+'''
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+logger = logging.getLogger('prawcore')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(handler)
+'''
 
 reddit_ro = praw.Reddit(client_id=secrets.reddit_client_id, client_secret=secrets.reddit_client_secret,
-                        user_agent='com.local.litwordbot:Python 3.8:v1.0 (by /u/lit_word_bot)')
-reddit_word_to_comment = hot_all_word_map(reddit_ro, None, 3, 1, 1, 1)
+                        user_agent='com.local.litwords:Python 3.8:v1.0 (by /u/lit_word_x)')
+reddit_word_to_comment = hot_all_word_map(reddit_ro, 100, 1, 1, 1, 1)
 reddit_set = set(reddit_word_to_comment.keys())
+reddit_stems = {stem(word): word for word in reddit_set}
 # some positive tests. These words should always appear in the output list.
 #reddit_set = reddit_set.union(set(["litten", "minaret", "effaces"]))
-print("Possible words, prior to any filtering: " + str(reddit_set))
+print("Possible words, prior to any filtering: " + str(sorted(list(reddit_set))))
+print("Possible stems, prior to any filtering: " + str(len(reddit_stems.keys())))
 
-stemmer = PorterStemmer()
-reddit_set_stem_map = {stemmer.stem(word): word for word in reddit_set}
+# These are Scrabble words intersected on a much larger frequency chart
+world_word_c = read_data("./frequency/world_scrabble_freq.txt", ":")
+print("Read world word counts.")
+# These are Scrabble words intersected on a much larger frequency chart
+corpus_word_c = read_data("./frequency/corpus_scrabble_freq.txt", ":")
+print("Read corpus word counts.")
+# Overlay the sets of Scrabble word frequencies
+merged_word_c = merge_map(world_word_c, corpus_word_c, lambda v1, v2: v1 + v2)
+print("Word counts merged.")
+# Convert the words to more general stems, and sum the connected frequencies
+merged_stem_c = word_freq_to_stem_freq(merged_word_c)
+print("Merged counts stemmed.")
+# For pruning stems
+corpus_stem_c = word_freq_to_stem_freq(corpus_word_c)
+# Keep only the Corpus word stems, but retain the overall frequencies
+pruned_stem_c = keep_only(
+    merged_stem_c, lambda k: k in corpus_stem_c)
+print("Pruned merged stems.")
 
-print("Reddit Not Stemmed Unique Count: " + str(len(reddit_set)))
-print("Reddit Stemmed Unique Count: " + str(len(reddit_set_stem_map.keys())))
+#Take only the rare stems from the pruned stem map
+rare_stems = set(filter(
+    lambda stem: pruned_stem_c[stem] <= 150000, pruned_stem_c.keys()))
+print("Filtered for rare stems.")
 
 offensive_set = word_set_from_dir_no_clean(
-    "./offensive", "offensive/", "windows-1252")
+    "./offensive", "offensive/", "utf-8")
 
-corpus_stem_dict = {}
-corpus_word_dict = {}
-
-txt_files = glob.glob("./" + "corpusfreq/" + "*.txt")
-with io.open(txt_files[0], "r", encoding="utf-8") as f:
-    for line in f:
-        w, c = line.split(":")
-        corpus_word_dict[w] = int(c)
-        w = stemmer.stem(clean(w))
-        if(w in corpus_stem_dict.keys()):
-            count = corpus_stem_dict[w]
-            corpus_stem_dict[w] = count + int(c)
-        else:
-            corpus_stem_dict[w] = int(c)
-
-corpus_word_set = corpus_word_dict.keys()
-
-# Rough frequency filter to get rid of words over X frequency, including
-# their various forms
-# 300,000 is permissive for now, to get an idea of rarities
-rare_stems = set(filterfalse(
-    lambda s: corpus_stem_dict[s] >= 70000, corpus_stem_dict.keys()))
-
-print("Corpus stem set filtered for rare stems size: " +
-      str(len(rare_stems)))
-
-# What do we gain for using the corpus?
-# We avoid "words" like "bloop" (blooper)
+# Find the overlap between the Reddit Stems and Rare Stems
 candidate_stems = rare_stems.intersection(
-    reddit_set_stem_map.keys()) - offensive_set
+    set(reddit_stems.keys())) - stem_set(offensive_set)
+print("Eliminated offensive words.")
 
-for candidate_stem in candidate_stems:
-    print("Calling API for candidate: " + reddit_set_stem_map[candidate_stem])
+#Convert the stemmed Reddit word back into its original word
+prefilter_words = [reddit_stems[stem] for stem in candidate_stems]
+#Filter for words that exist in the Scrabble frequencies
+postfilter_words = list(filter(
+    lambda word: word in merged_word_c.keys(), prefilter_words))
 
-# This is going to be cleaned up & simplified but for now it is what it is
-# This could be made !!!!WAY!!!! more abstract to pull in many sources.
-# Existing desire: if(isWorthy(word)): post(word, Comment)
-# That line above is literally all that matters here.
+print("Eliminated false words.")
+
+print("Winners:\n\n")
+print(postfilter_words)
+
+blocker = input("Enter anything to continue.")
 
 final_results = []
-for w in candidate_stems:
+for w in postfilter_words:
     # Expand the stem back to its original word
-    real_word = reddit_set_stem_map[w]
+    real_word = reddit_stems[w]
     print("Working on: " + real_word)
     response = requests.get("https://www.dictionaryapi.com/api/v3/references/collegiate/json/" +
                             real_word + "?key=" + secrets.webster_dict_key)
@@ -334,11 +383,11 @@ for w in candidate_stems:
     if(first_def_meta["offensive"]):
         continue  # obviously don't define offensive words
     elif (first_def_meta["id"].lower() != first_def_meta["id"]):
-        continue  # probably a name or an acronym
+        continue  # probably a name, acronym, or mashup of words
 
     # get rid of phrasal variants
-    real_variants = list(filterfalse(lambda variant: len(
-        variant.split()) > 1, first_def_meta['stems']))
+    real_variants = list(filter(lambda variant: len(
+        variant.split()) == 1, first_def_meta['stems']))
 
     # add current word to variants
     real_variants.append(real_word)
@@ -350,11 +399,13 @@ for w in candidate_stems:
 
     real_freq = 0
     for variant in real_variants:
-        if variant in corpus_word_dict:
-            variant_frequency = corpus_word_dict[variant]
+        if variant in world_word_c:
+            variant_frequency = world_word_c[variant]
             print("found variant in frequencies: " +
                   variant + " : " + str(variant_frequency))
-            real_freq += corpus_word_dict[variant]
+            real_freq += variant_frequency
+        else:
+            real_freq += 12711  # lowest freq in the list
 
     if real_freq != 0 and "shortdef" in first_def and len(first_def["shortdef"]) != 0:
         def_field = first_def["shortdef"]
@@ -366,6 +417,8 @@ for w in candidate_stems:
         print(first_def_meta['id'].split(":")[
               0] + " || stem-reduced rarity score: " + str(rarity_score))
         print("\n\n\n")
+    else:
+        continue
 
 # This is what all that work was for: the rarest word, associated with the best comment
 final_results = sorted(final_results, key=lambda x: x[1])
